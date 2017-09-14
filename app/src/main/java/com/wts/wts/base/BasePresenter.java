e38401ed8af2c54c94bae09d670955af98c04d5e
hello@webskitters.com <hello@webskitters.com>
2017-09-14 15:53:37 +0530

    second commit

--- app/src/main/java/com/wts/wts/base/BasePresenter.java
+++ app/src/main/java/com/wts/wts/base/BasePresenter.java
@@ -1,9 +1,20 @@
 package com.wts.wts.base;
 
+import android.view.View;
+
 import com.wts.wts.mvp.view.BaseView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.util.concurrent.TimeoutException;
 
 import javax.inject.Inject;
 
+import okhttp3.ResponseBody;
+import retrofit2.HttpException;
 import rx.Observable;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
@@ -29,6 +40,31 @@ public class BasePresenter<V extends BaseView> {
             return false;
         } else {
             return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
+    protected void handleHTTPException(Throwable e){
+
+        getView().hideDialog();
+        if (e instanceof HttpException) {
+            try {
+                ResponseBody body = ((HttpException) e).response().errorBody();
+                JSONObject jObj=new JSONObject(body.string());
+                getView().showErrorMsg("Error: "+jObj.optString("status")+" "+jObj.optString("message"));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (JSONException e1) {
+                e1.printStackTrace();
+            }
+        }
+        else if(e instanceof IOException || e instanceof ConnectException){
+            getView().showErrorMsg("No Connection available.");
+        }
+        else if(e instanceof TimeoutException){
+            getView().showErrorMsg("Server could not be reached at this moment.");
+        }
+        else{
+            getView().showErrorMsg("Oops! Something went wrong. Try again later.");
         }
     }
 }
